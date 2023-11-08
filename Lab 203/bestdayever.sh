7397053885683768bdeb2b5b4fe4f7425f10504d
CBryan89 <147670949+CBryan89@users.noreply.github.com>
2023-11-07 18:49:24 -0600

    Update bestdayever.sh

---
+++ Lab 203/bestdayever.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+name=$1
+compliment=$2
+
+user=$(whoami)
+date=(date)
+whereami=$(pwd)
+
+
+echo "Good morning $name."
+sleep 1
+echo "At least you woke up today, $name."
+sleep 2
+echo "You have the best $compliment I've ever seen $name!"
+sleep 2
+echo "You are currently logged in as $user at $date on $whereami."
