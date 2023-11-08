481eba51e7689f3d13608975be6383a14a784faf
Chris Bryan <cbryan9891@gmail.com>
2023-11-07 19:31:45 -0600

    Everything up-to-date

--- Lab 203/bestdayever.sh
+++ Lab 203/bestdayever.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 name=$1
-compliment=$2
+complimnet=$2
 
 user=$(whoami)
-date=(date)
+date=$(date)
 whereami=$(pwd)
-
 
 echo "Good morning $name."
 sleep 1
-echo "At least you woke up today, $name."
-sleep 2
-echo "You have the best $compliment I've ever seen $name!"
-sleep 2
-echo "You are currently logged in as $user at $date on $whereami."
+echo "You have the best $compliment."
+sleep 1
+echo "You are currently logged in at $user on $whereami. Today is $date."
+
