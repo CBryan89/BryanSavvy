7ccb786dfb71934f6448fb4dc847115310fc42fb
cbryan9891 <cbryan9891@SavvyTest.myguest.virtualbox.org>
2023-11-15 18:13:49 -0600

    Everything up-to-date

---
+++ Lab 203/Ops6.sh
@@ -0,0 +1,15 @@
+#!/bit/bash
+
+echo "Enter a website."
+read website
+
+function gather_info() {
+    whois $website >> whois.txt
+    echo "-----" >> whois.txt
+    dig $website >> whois.txt
+    echo "-----" whois.txt
+    host $website >> whois.txt
+    echo "-----" >> whois.txt
+    nslookup $website >> whois.txt
+}
+gather_info
