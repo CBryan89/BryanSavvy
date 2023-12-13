bb35c7bb061d716a8de833fbec843ed69dcbc838
cbryan9891 <cbryan9891@SavvyTest.myguest.virtualbox.org>
2023-12-12 19:10:27 -0600

    yes

--- Lab 204/Ops2.py
+++ Lab 204/Ops2.py
@@ -8,7 +8,16 @@
 # Greater than: a > b
 # Greater than or equal to: a >= b
 # Create an if statement using a logical conditional of your choice and include elif keyword that executes when other conditions are not met.
+print("Enter two numbers.")
+a = input ("Enter a number for a ")
+b = input ("Enter a number for b ")
 
+if (a == b) and (b == a):
+    print ("a is equal to b")
+elif (a < b) and (a <= b):
+    print ("a is less than b")
+else:
+    print ("a is greater than b")
 # Create an if statement that includes both elif and else to execute when both if and elif are not met.
 
 # Stretch Goals (Optional Objectives)
@@ -20,4 +29,14 @@
 
 # Create a nested if statement.
 
+x = 11
+if x > 10:
+    print("Above ten,")
+    if x > 20:
+        print("and also above 20.")
+    else:
+        print("but not above 20.")
 # Create an if statement that includes pass to avoid errors.
+
+# if b > a:
+#   pass
