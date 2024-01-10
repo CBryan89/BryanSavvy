0fbb481503fb73057a28751451029e7e034725c2
cbryan9891 <cbryan9891@SavvyTest.myguest.virtualbox.org>
2024-01-09 18:46:53 -0600

    Yes

---
+++ Lab 204/Ops8.py
@@ -0,0 +1,18 @@
+# Your task is very simple here: write a program that uses a for loop to "count mississippi" to five.
+#  Having counted to five, 
+# the program should print to the screen the final message "Ready or not, here I come!"
+import time
+
+#Start code below this line:
+
+import time
+
+#Start code below this line:
+
+
+mississippi = 1 
+for mississippi in range(1,6):
+    print(mississippi, "Mississippi")
+    time.sleep(1)
+    pass
+print("Ready or not here  I come")
